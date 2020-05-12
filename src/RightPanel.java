cd479acf0db9b81162792b9b65b83c5403e95ec1
mammad <m.mahdisaeedi99@gmail.com>
2020-05-12 17:05:14 +0430

    Designed third panel for show response of request

--- src/RightPanel.java
+++ src/RightPanel.java
@@ -1,6 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class RightPanel extends JPanel  {
+public class RightPanel extends JPanel {
     public RightPanel() {
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new FlowLayout());
+        JLabel status = new JLabel("200 OK");
+        JLabel runtime = new JLabel("1.00 s");
+        JLabel volume = new JLabel("100.0 KB");
+        String history[]={"A few minutes ago"};
+        JComboBox historyBox = new JComboBox<>(history);
+        infoPanel.add(status);
+        infoPanel.add(runtime);
+        infoPanel.add(volume);
+        infoPanel.add(historyBox);
+
+        JPanel preview = new JPanel();
+        JPanel header = new JPanel();
+        JPanel cookie = new JPanel();
+        JPanel timeLine = new JPanel();
+        JTabbedPane tabbedPane = new JTabbedPane();
+        tabbedPane.setBounds(50, 50, 200, 200);
+        tabbedPane.add("Preview", preview);
+        tabbedPane.add("Header", header);
+        tabbedPane.add("Cookie", cookie);
+        tabbedPane.add("TimeLine", timeLine);
+
+        this.setLayout(new BorderLayout());
+        this.add(infoPanel, BorderLayout.NORTH);
+        this.add(tabbedPane, BorderLayout.CENTER);
     }
 }
